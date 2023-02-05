ba1adf8c3f2a3009f7d371bccb4c08e8f0f6d528
lonely011 <120551291+lonely011@users.noreply.github.com>
2023-02-05 14:29:20 +0700

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -58,9 +58,6 @@ async def ping_fallen(_, message: Message):
             [
                 [
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
-                    InlineKeyboardButton(
-                        "✨ sᴏᴜʀᴄᴇ ✨",
-                        url="https://github.com/lonely011/FallenMusic",
                     ),
                 ],
             ]
